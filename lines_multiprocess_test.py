8a1887d8791ae1ec5976c954801cc12c4131a624
Chris-Edit <45399752+Chris-Edit@users.noreply.github.com>
2024-02-26 10:44:23 +0900

    Update lines_multiprocess_test.py

--- lines_multiprocess_test.py
+++ lines_multiprocess_test.py
@@ -33,43 +33,16 @@ if __name__ == '__main__':
 
     start = time.time()
 
-    # pool = multiprocessing.Pool(processes=6)
-    # pool.map(gh_run, range(100))
-    # pool.close()
-    # pool.join()
+    i = 100
+    
+    pool = multiprocessing.Pool(processes=6)
+    pool.map(gh_run, range(100))
+    pool.close()
+    pool.join()
     
 
-    for i in range(100):
-        gh_run(i)
-
-
+    # for i in range(100):
+    #     gh_run(i)
 
     test = time.time() - start
     print(test * 10000)
-
-    # with multiprocessing.Pool(processes=2) as pool:
-    #     outputs = pool.starmap(gh_run, ['twisty.gh'])
-
-    # import time
-    # start = time.time()
-    # 
-    # for _ in range(8):
-    #     output = gh_run('twisty.gh')
-    # 
-    # print((time.time() - start) * 100000)
-
-    # decode results
-    # output = outputs[0]
-    # branch = output['values'][0]['InnerTree']['{0;0}']
-    # lines = [rhino3dm.CommonObject.Decode(json.loads(item['data'])) for item in branch]
-    # 
-    # filename = 'twisty.3dm'
-    # 
-    # print('Writing {} lines to {}'.format(len(lines), filename))
-    # 
-    # # create a 3dm file with results
-    # model = rhino3dm.File3dm()
-    # for l in lines:
-    #     model.Objects.AddCurve(l) # they're actually LineCurves...
-    # 
-    # model.Write(filename)
